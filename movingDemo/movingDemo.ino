9b57fe45e5da737cf7018228e4886823652ee3f6
noe98 <steelersfan437@gmail.com>
2018-03-12 17:51:14 -0400

    Fix Move Demo
    I (Think I) successfully adapted the code to use four motors instead of 2.

--- movingDemo/movingDemo.ino
+++ movingDemo/movingDemo.ino
@@ -1,14 +1,35 @@
+//Motor A [Front Right]
+int enA = 10
+int in1 = 9
+int in2 = 8
+//Motor B [Front Left]
+int enB = 5
+int in3 = 7
+int in4 = 6
+//Motor C [Back Right]
+int enC = 15
+int in5 = 14
+int in6 = 13
+//Motor D [Back Left]
+int enD = 20
+int in7 = 19
+int in8 = 18
+
 void setup() {
   // put your setup code here, to run once:
   // set all the motor control pins to outputs
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(enC, OUTPUT);
-  pinMode(enD, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  pinMode(enA, OUTPUT); //Engine A
+  pinMode(enB, OUTPUT); //Engine B
+  pinMode(enC, OUTPUT); //Engine C
+  pinMode(enD, OUTPUT); //Engine D
+  pinMode(in1, OUTPUT); //A
+  pinMode(in2, OUTPUT); //A
+  pinMode(in3, OUTPUT); //B
+  pinMode(in4, OUTPUT); //B
+  pinMode(in5, OUTPUT); //C
+  pinMode(in6, OUTPUT); //C
+  pinMode(in7, OUTPUT); //D
+  pinMode(in8, OUTPUT); //D
 }
 
 void moveForward(int speed){
@@ -22,9 +43,19 @@ void moveForward(int speed){
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
 
+  // turn on motor C
+  digitalWrite(in5, HIGH);
+  digitalWrite(in6, LOW);
+
+  // turn on motor D
+  digitalWrite(in7, HIGH);
+  digitalWrite(in8, LOW);
+
   // set speed within a possible range 0~255
   analogWrite(enA, speed);
   analogWrite(enB, speed);
+  analogWrite(enC, speed);
+  analogWrite(enD, speed);
 }
 
 void moveBackwards(int speed){
@@ -38,9 +69,19 @@ void moveBackwards(int speed){
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
 
+  // turn on motor C
+  digitalWrite(in5, LOW);
+  digitalWrite(in6, HIGH);
+
+  // turn on motor D
+  digitalWrite(in7, LOW);
+  digitalWrite(in8, HIGH);
+
   // set speed within a possible range 0~255
   analogWrite(enA, speed);
   analogWrite(enB, speed);
+  analogWrite(enC, speed);
+  analogWrite(enD, speed);
 }
 
 void moveLeft(int speed){
@@ -54,9 +95,19 @@ void moveLeft(int speed){
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
 
+  // turn on motor C
+  digitalWrite(in5, HIGH);
+  digitalWrite(in6, LOW);
+
+  // turn on motor D
+  digitalWrite(in7, LOW);
+  digitalWrite(in8, LOW);
+
   // set speed within a possible range 0~255
   analogWrite(enA, speed);
   analogWrite(enB, speed);
+  analogWrite(enC, speed);
+  analogWrite(enD, speed);
 }
 
 void moveRight(int speed){
@@ -70,9 +121,19 @@ void moveRight(int speed){
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
 
+  // turn on motor C
+  digitalWrite(in5, LOW);
+  digitalWrite(in6, LOW);
+
+  // turn on motor D
+  digitalWrite(in7, HIGH);
+  digitalWrite(in8, LOW);
+
   // set speed within a possible range 0~255
   analogWrite(enA, speed);
   analogWrite(enB, speed);
+  analogWrite(enC, speed);
+  analogWrite(enD, speed);
 }
 
 void moveStop(){
@@ -81,6 +142,10 @@ void moveStop(){
   digitalWrite(in2, LOW);  
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
+  digitalWrite(in5, LOW);
+  digitalWrite(in6, LOW);  
+  digitalWrite(in7, LOW);
+  digitalWrite(in8, LOW);
 
   delay(200);
 }
