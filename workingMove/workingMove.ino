108808da5a26cdd859e3062cbef0dff47c3dd548
William McCann <williammccann@Williams-MacBook-Pro-2.local>
2018-03-21 16:58:43 -0400

    Refactored existing code

--- workingMove/workingMove.ino
+++ workingMove/workingMove.ino
@@ -1,8 +1,8 @@
+// Define pins for output
 int la = 2;
 int lb = 3;
 int ra = 4;
 int rb = 5;
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,35 +11,44 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int aspeed = analogRead(A0) - 500;
-  int bspeed = analogRead(A1) - 531;
+  mapJoystick(analogRead(A0), analogRead(A1));
+}
+
+void adjustLeftRight(int left, int right) {
+    /* Helper method for mapJoystick which writes to pins
+     * Precondition: takes values returned by constrain function
+     * Postcondition: no return, writes to pins
+     */
+    if((left != 0) and (right != 0)){
+      analogWrite(la, 120 + left);
+      analogWrite(lb, 120 - left);
+      analogWrite(ra, 120 + right);
+      analogWrite(rb, 120 - right);
+    }else{
+      analogWrite(la, 0);
+      analogWrite(lb, 0);
+      analogWrite(ra, 0);
+      analogWrite(rb, 0);
+    }
+    Serial.print(left);
+    Serial.print(", ");
+    Serial.println(right);
+  }
+
+void mapJoystick(int readAnalog0, int readAnalog1) {
+  /* Reads analog values from joystick, calling helper to write to out-pins
+   * Precondition: must have analogRead input
+   * Postcondition: returns nothing
+   */  
+  int aspeed = readAnalog0 - 500;
+  int bspeed = readAnalog1 - 531;
   if (abs(aspeed) < 5){aspeed = 0;}
   if (abs(bspeed) < 5){bspeed = 0;}
   aspeed = map(aspeed, -420, 420, -100, 100);
   bspeed = map(bspeed, -420, 420, -100, 100);
   int left = (aspeed + bspeed);
   int right = (aspeed - bspeed);
-  left = constrain(left, -200,200);
-  right = constrain(right, -200,200);
-
-  if((left != 0) and (right != 0)){
-    analogWrite(la, 120 + left);
-    analogWrite(lb, 120 - left);
-    analogWrite(ra, 120 + right);
-    analogWrite(rb, 120 - right);
-  }
-  else{
-    analogWrite(la, 0);
-    analogWrite(lb, 0);
-    analogWrite(ra, 0);
-    analogWrite(rb, 0);
-  }
+  adjustLeftRight(constrain(left, -200, 200), constrain(right, -200, 200));
   
-
-  Serial.print(left);
-  Serial.print(", ");
-  Serial.println(right);
-  
-
 }
 
