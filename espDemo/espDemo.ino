c62ec096d056a3b9a5746642ef71293e0d301f58
noe98 <steelersfan437@gmail.com>
2018-04-03 16:17:56 -0400

    ESP Connection
    Code to connect to my hotspot.

--- espDemo/espDemo.ino
+++ espDemo/espDemo.ino
@@ -1,9 +1,17 @@
+#include <ESP8266WiFi.h>
+const char* ssid = "G iphone";
+const char* password  = "justinsucks";
+
+
 void setup() {
   // put your setup code here, to run once:
 
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
